110296288f1812f4542892f5d391570d62b1383b
Werikson1 <ettone166@gmail.com>
2023-05-04 19:49:16 -0300

    teste

--- dag_factory/main.py
+++ dag_factory/main.py
@@ -3,34 +3,27 @@ import yaml
 import argparse
 from generate import Handler
 
-
 def loadconfig(path: str, config: str) -> any:
     with open(path + '/' + config) as f:
         content = f.read()
     return yaml.safe_load(content)
 
-def invoke(json_config: any) -> None:
-    Handler(json_config).execute()
+def invoke(json_config: any, output_path: str) -> None:
+    Handler(json_config, output_path).execute()
 
 if __name__ == "__main__":
     os.environ['CONFIG_PATH'] = '/config'
     os.environ['TEMPLATE_PATH'] = '/templates'
     os.environ['DAG_RAW_TEMPLATE'] = 'template_raw.j2'
     os.environ['DAG_TRUSTED_TEMPLATE'] = 'template_trusted.j2'
-    # os.environ['DAG_TEMPLATE'] = 'template.j2'
     os.environ['DAG_FOLDER'] = 'dags/generated/'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="Dag config filename")
+    parser.add_argument("--input", type=str, required=True, help="Input YAML file path")
+    parser.add_argument("--output", type=str, required=True, help="Output DAG file path")
     args = parser.parse_args()
 
     path = os.path.dirname(os.path.abspath(__file__)) + os.environ['CONFIG_PATH']
 
-    if args.config is not None:
-        json_config = loadconfig(path, args.config)
-        invoke(json_config)
-
-    else:
-        for config in os.listdir(path):
-            json_config = loadconfig(path, config)
-            invoke(json_config)
+    json_config = loadconfig(path, args.input)
+    invoke(json_config, args.output)
