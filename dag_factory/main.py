5e4ea49b2b299a4a13000f7e185adaecbd3cf345
Werikson Rodrigues <49258737+Werikson1@users.noreply.github.com>
2023-05-04 20:56:15 -0300

    Update main.py

--- dag_factory/main.py
+++ dag_factory/main.py
@@ -8,23 +8,23 @@ def loadconfig(path: str, config: str) -> any:
         content = f.read()
     return yaml.safe_load(content)
 
-def invoke(json_config: any) -> None:
-    Handler(json_config).execute()
+def invoke(json_config: any, output: str) -> None:
+    Handler(json_config, output).execute()
 
 if __name__ == "__main__":
     os.environ['CONFIG_PATH'] = '/config'
     os.environ['TEMPLATE_PATH'] = '/templates'
     os.environ['DAG_RAW_TEMPLATE'] = 'template_raw.j2'
     os.environ['DAG_TRUSTED_TEMPLATE'] = 'template_trusted.j2'
-    os.environ['DAG_FOLDER'] = 'dags/generated/'
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", nargs='+', help="Dag config filename(s)", required=True)
-    parser.add_argument("-o", "--output", help="Output path for generated DAG files", required=True)
+    parser.add_argument("-o", "--output", help="Output folder for generated DAG files", default='dags/generated/')
     args = parser.parse_args()
+
+    output = args.output
 
     path = os.path.dirname(os.path.abspath(__file__)) + os.environ['CONFIG_PATH']
 
-    for config in args.input:
-        json_config = loadconfig(path, os.path.basename(config))
-        invoke(json_config)
+    for config in os.listdir(path):
+        json_config = loadconfig(path, config)
+        invoke(json_config, output)
