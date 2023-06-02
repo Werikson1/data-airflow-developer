01d450c5b9ab201f06491d849a87b2514cd37443
Werikson Rodrigues <49258737+Werikson1@users.noreply.github.com>
2023-06-01 21:55:27 -0300

    Update test_gb_external_task_sensor.py

--- tests/sensor/test_gb_external_task_sensor.py
+++ tests/sensor/test_gb_external_task_sensor.py
@@ -7,6 +7,7 @@ from airflow.utils.state import DagRunState
 from airflow.utils.timezone import datetime
 from airflow.utils.types import DagRunType
 from airflow.exceptions import AirflowException
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -21,10 +22,10 @@ DEV_NULL = "/dev/null"
 
 class TestGbExternalTaskSensor:
     def setup_method(self):
-    self.dagbag = DagBag(dag_folder=DEV_NULL, include_examples=False)
-    self.args = {"owner": "airflow", "start_date": DEFAULT_DATE}
-    settings.engine = create_engine('sqlite:///:memory:', echo=True)
-    settings.Session = sessionmaker(bind=settings.engine)
+        self.dagbag = DagBag(dag_folder=DEV_NULL, include_examples=False)
+        self.args = {"owner": "airflow", "start_date": DEFAULT_DATE}
+        settings.engine = create_engine('sqlite:///:memory:', echo=True)
+        settings.Session = sessionmaker(bind=settings.engine)
 
 
     def create_dag_runs(self, dag, config):
